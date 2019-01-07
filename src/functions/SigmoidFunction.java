e74e9aca2b6603acae0370898ccd725b846188b7
Stanisław Zakrzewski <stanislaw.zakrzewski13@gmail.com>
2019-01-07 20:42:19 +0100

    Sigmoid derivative is simpler

--- src/functions/SigmoidFunction.java
+++ src/functions/SigmoidFunction.java
@@ -3,11 +3,11 @@ package functions;
 public class SigmoidFunction implements Function {
     @Override
     public double functionValue(double x) {
-        return 1 / (1 + Math.exp(-x));
+        return 1.0 / (1.0 + Math.exp(-x));
     }
 
     @Override
     public double derivativeValue(double x) {
-        return (Math.exp(x) / Math.pow((Math.exp(x) + 1), 2));
+        return functionValue(x) * (1- functionValue(x));
     }
 }
